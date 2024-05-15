96d6fc1d115834dc015920063bf736e39a8d84a0
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-15 20:59:58 -0300

    feat: criando arquivo para layout base

---
+++ src/shared/layouts/LayoutBasePage.tsx
@@ -0,0 +1,16 @@
+
+
+interface ILayoutBasePagesProps {
+    children: React.ReactNode;
+    title: string;
+    toobar?: React.ReactNode;
+};
+
+export const LayoutBasePages: React.FC<ILayoutBasePagesProps> = ({ children, title, toobar }) => {
+
+
+    return (
+       <></>
+    );
+    
+}
