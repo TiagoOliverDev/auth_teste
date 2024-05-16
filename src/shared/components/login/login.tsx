817345cd8ff6c4126e9fa3a3eeab714a1996a724
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-15 21:28:57 -0300

    feat: código base da tela de login

--- src/shared/components/login/login.tsx
+++ src/shared/components/login/login.tsx
@@ -1,10 +1,51 @@
 
+import { Box, Button, Card, CardActions, CardContent, CircularProgress, TextField, Typography } from "@mui/material"
+// import { useAuthContext } from "../../contexts";
+// import { useState } from "react";
+// import * as yup from 'yup'
 
 export const Login: React.FC = () => {
 
     
     return(
-        <>
-        </>
+        <Box width={'100vw'} height={'100vh'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
+            
+        <Card>
+            <CardContent>
+                <Box display={'flex'} flexDirection={'column'} gap={2} width={350}>
+                    <Typography variant="h6" align="center">Login</Typography>
+
+                    <TextField
+                        fullWidth
+                        type="email"
+                        label='Email'
+                    />
+
+                    <TextField
+                        fullWidth
+                        label='Senha'
+                        type="password"
+
+                    />
+
+
+                </Box>
+            
+            </CardContent>
+
+                <CardActions>
+                    <Box width={'100%'} display={'flex'} justifyContent={'center'}>
+
+                        <Button
+                            variant="contained"
+                        >
+                            Entrar
+                        </Button>
+
+                    </Box>
+                </CardActions>
+        </Card>
+
+    </Box>
     )
 }
