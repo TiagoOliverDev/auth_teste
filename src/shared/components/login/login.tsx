bb2d3f5aacea609621e0bbd44548fe66315d614d
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-15 21:59:41 -0300

    feat: estiliazando com tailwind

--- src/shared/components/login/login.tsx
+++ src/shared/components/login/login.tsx
@@ -1,51 +1,38 @@
-
-import { Box, Button, Card, CardActions, CardContent, CircularProgress, TextField, Typography } from "@mui/material"
-// import { useAuthContext } from "../../contexts";
-// import { useState } from "react";
-// import * as yup from 'yup'
+import React from 'react';
+import { Box, Card, CardContent, CardActions, Typography, TextField, Button } from '@mui/material';
 
 export const Login: React.FC = () => {
+  return (
+    <Box className="w-screen h-screen flex items-center justify-center">
+      <Card>
+        <CardContent>
+          <Box className="flex flex-col gap-2 w-72">
+            <Typography variant="h6" align="center">b2bit</Typography>
 
-    
-    return(
-        <Box width={'100vw'} height={'100vh'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
-            
-        <Card>
-            <CardContent>
-                <Box display={'flex'} flexDirection={'column'} gap={2} width={350}>
-                    <Typography variant="h6" align="center">Login</Typography>
+            <TextField
+              fullWidth
+              type="email"
+              label='Email'
+            />
 
-                    <TextField
-                        fullWidth
-                        type="email"
-                        label='Email'
-                    />
+            <TextField
+              fullWidth
+              label='Senha'
+              type="password"
+            />
+          </Box>
+        </CardContent>
 
-                    <TextField
-                        fullWidth
-                        label='Senha'
-                        type="password"
-
-                    />
-
-
-                </Box>
-            
-            </CardContent>
-
-                <CardActions>
-                    <Box width={'100%'} display={'flex'} justifyContent={'center'}>
-
-                        <Button
-                            variant="contained"
-                        >
-                            Entrar
-                        </Button>
-
-                    </Box>
-                </CardActions>
-        </Card>
-
+        <CardActions>
+          <Box className="w-full flex justify-center">
+            <Button
+              variant="contained"
+            >
+              Entrar
+            </Button>
+          </Box>
+        </CardActions>
+      </Card>
     </Box>
-    )
-}
+  );
+};
