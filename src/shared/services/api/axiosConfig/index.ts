2fe953aebc594695b9aa5ab7777ee1ad801c76fd
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-18 10:35:04 -0300

    feat: ajustando interceptors

--- src/shared/services/api/axiosConfig/index.ts
+++ src/shared/services/api/axiosConfig/index.ts
@@ -1,27 +1,39 @@
-import axios from "axios";
 import { errorInterceptor, responseInterceptor } from "./interceptors";
+import axios, { AxiosInstance, AxiosError, AxiosResponse } from "axios";
 import { Enviroment } from "../../../environment";
 
-// Criação da instância do Axios sem definir o header 'Authorization' aqui
-const API = axios.create({
-    baseURL: Enviroment.URL_BASE,
-});
-
-// Interceptor de requisição para adicionar o token JWT a cada requisição
-API.interceptors.request.use((config) => {
+// Função para adicionar o token JWT a cada requisição
+const addTokenToRequest = (config: any) => {
     const token = localStorage.getItem('APP_ACCESS_TOKEN');
     if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
-}, (error) => {
+};
+
+// Função para tratar erros de resposta
+const handleResponseError = (error: AxiosError) => {
+    // Aqui você pode adicionar lógica para tratar erros de resposta
     return Promise.reject(error);
+};
+
+// Criação da instância do Axios sem definir o header 'Authorization' aqui
+const API = axios.create({
+    baseURL: Enviroment.URL_BASE,
+    headers: {
+        'Accept': 'application/json;version=v1_web',
+        'Content-Type': 'application/json'
+    }
 });
 
-// Interceptors de resposta já existentes
+
+// Adicionando interceptor de requisição
+API.interceptors.request.use(addTokenToRequest, (error) => Promise.reject(error));
+
+// Adicionando interceptor de resposta
 API.interceptors.response.use(
-    (response) => responseInterceptor(response),
-    (error) => errorInterceptor(error),
+    (response: AxiosResponse) => response,
+    handleResponseError
 );
 
 export { API };
