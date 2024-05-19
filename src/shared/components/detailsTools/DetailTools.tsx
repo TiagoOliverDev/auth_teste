0f97287bcca2f3f4d88dcadc808403ae48bcb6c0
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-19 20:07:53 -0300

    feat: ajustando barra details

--- src/shared/components/detailsTools/DetailTools.tsx
+++ src/shared/components/detailsTools/DetailTools.tsx
@@ -1,48 +1,39 @@
-import { Box, Button, Icon, Paper, useTheme, Divider, Skeleton, Typography, useMediaQuery, Theme } from "@mui/material";
+import { Box, Button, Icon, Paper, Typography} from "@mui/material";
 import { IDetailToolsProps } from "../../../@types/IDetailToolsProps";
-import SaveIcon from '@mui/icons-material/Save';
+import KeyboardTabIcon from '@mui/icons-material/KeyboardTab';
 
 
 export const DetailTools: React.FC<IDetailToolsProps> = ({
-    textButtonLogout = "Logout",
+    textButtonLogout = " ",
     showButtonLogout = false,
     whenCilickingButtonLogout,
 }) => {
 
-    // const theme = useTheme();
-    // const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
-    // const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
-
     return (
         <Box
-            // height={theme.spacing(5)}
-            marginX={1}
-            padding={1}
-            paddingX={2}
-            display={"flex"}
-            gap={1}
-            alignItems={"center"}
             component={Paper}
+            className="mx-1 p-1 px-2 flex gap-1 items-center overflow-hidden"
         >
-            {(showButtonLogout) && (
-                <Button
-                    className="bg-[#02274F] text-white py-2 px-4 flex items-center gap-2 rounded shadow-none hover:bg-[#011f3e] focus:outline-none"
-                    variant="contained"
-                    color="primary"
-                    disableElevation
-                    onClick={whenCilickingButtonLogout}
-                    startIcon={ <Icon> <SaveIcon /> </Icon> }
-                >
-                    <Typography variant="button" whiteSpace={"nowrap"} textOverflow={"ellipsis"} overflow={"hidden"}>
-                        {textButtonLogout}
-                    </Typography>
-                </Button>
+            <Box className="flex-grow" /> 
+            {showButtonLogout && (
+              <Button
+                className="w-[385.88px] h-[54px] flex items-center"
+                style={{backgroundColor: '#02274F', borderRadius: 9, marginLeft: 'auto'}}
+                variant="contained"
+                color="primary"
+                disableElevation
+                onClick={whenCilickingButtonLogout}
+                endIcon={
+                    <Icon style={{ height: '24px', display: 'flex', alignItems: 'center' }}>
+                        <KeyboardTabIcon/>
+                    </Icon>
+                }
+              >
+                <Typography variant="button" whiteSpace={"nowrap"} textOverflow={"ellipsis"} overflow={"hidden"}>
+                  {textButtonLogout}
+                </Typography>
+              </Button>
             )}
-
-            {/* {showButtonLogout && (
-                <Skeleton width={109} height={60} />
-            )} */}
-
         </Box>
     );
 };
