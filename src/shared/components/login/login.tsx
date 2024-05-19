9591a4ba7ba813d32330c7db0e345cc04f59db37
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-19 17:03:15 -0300

    feat: ajustando estilização

--- src/shared/components/login/login.tsx
+++ src/shared/components/login/login.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Box, Card, CardContent, CardActions, Typography, TextField, Button, CircularProgress } from '@mui/material';
+import { Box, Card, CardContent, CardActions, Typography, TextField, Button, CircularProgress, InputLabel } from '@mui/material';
 import { useState } from "react";
 import { useAuthContext } from '../../contexts';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+import '../../../index.css'
+
+
+const b2bitLogo = 'LOGOTIPO.svg'
 
 const loginSchema = yup.object().shape({
   email: yup.string().email('Email inválido').required('Email é obrigatório'),
@@ -36,44 +40,83 @@ export const Login: React.FC = () => {
   return (
     <Box className="w-screen h-screen flex items-center justify-center">
       <Card 
-        className='w-[534px] h-[438px]'
+        className='w-[438px] h-[534px]'
         style={{borderRadius: 18}}
       >
         {/* <CardContent style={{width: 385.88, height: 288, top: 205.88, left: 25.88, borderRadius: 9}}> */}
-        <CardContent >
+        <CardContent>
           <Box className="flex flex-col items-center justify-center gap-y-8 w-full h-full">
-            <Typography variant="h6" align="center">b2bit</Typography>
+            <img src={b2bitLogo} alt="B2bit Logo" style={{ width: 309.6, height: 94.81, marginBottom: '33px', marginTop: '43px' }} />
+          </Box>
 
+          <Box>
             <form onSubmit={formik.handleSubmit}>
-              <TextField
-                fullWidth
-                type="email"
-                label='E-mail'
-                {...formik.getFieldProps('email')}
-                disabled={isLoading}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email}
-              />
+            <InputLabel
+              htmlFor="Idemail"
+              style={{
+                marginBottom: '9px',
+                fontSize: '18px', 
+                fontWeight: 700, 
+                lineHeight: '22.5px',
+                textAlign: 'left',
+                color: '#262626'
+              }}
+            >
+              E-mail
+            </InputLabel>
+            <TextField
+              role="E-mail"
+              id='Idemail'
+              fullWidth
+              type="email"
+              // label="E-mail"
+              multiline
+              variant="filled"
+              placeholder="@gmail.com"
+              {...formik.getFieldProps('email')}
+              disabled={isLoading}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+              style={{ marginBottom: '25px', borderRadius: 15 }} 
+            />
 
-              <TextField
-                fullWidth
-                label='Senha'
-                type="password"
-                {...formik.getFieldProps('password')}
-                disabled={isLoading}
-                error={formik.touched.password && Boolean(formik.errors.password)}
-                helperText={formik.touched.password && formik.errors.password}
-              />
-
+            <InputLabel
+              htmlFor="Idpassword"
+              style={{
+                marginBottom: '9px',
+                fontSize: '18px', 
+                fontWeight: 700, 
+                lineHeight: '22.5px',
+                textAlign: 'left',
+                color: '#262626'
+              }}
+            >
+              Password
+            </InputLabel>
+            <TextField
+              role="Password"
+              id='Idpassword'
+              fullWidth
+              type="password"
+              // label="Password"
+              multiline
+              variant="filled"
+              placeholder="********"
+              {...formik.getFieldProps('password')}
+              disabled={isLoading}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+              style={{ marginBottom: '32px', borderRadius: 15 }} 
+            />
               <CardActions>
                 <Box className="w-full flex justify-center">
                   <Button
                     className="w-[385.88px] h-[54px] flex items-center"
-                    // style={{backgroundColor: '#02274F', borderRadius: 9}}
+                    style={{backgroundColor: '#02274F', borderRadius: 9}}
                     variant="contained"
                     disabled={isLoading}
                     type="submit"
-                    endIcon={isLoading ? <CircularProgress size={20} variant="indeterminate" role="progressbar" color="inherit" /> : undefined}
+                    endIcon={isLoading ? <CircularProgress size={20} variant="indeterminate" role="progressbar" color="primary" /> : undefined}
                   >
                     Sign In
                   </Button>
